78e9f26a7ab66e33077c97cbe2dc9bdb0a8b1937
michael <r0380573@student.thomasmore.be>
2017-10-23 13:13:54 +0200

    bewegen van plaats selectie

--- VierOpEenRij.py
+++ VierOpEenRij.py
@@ -2,20 +2,20 @@
 import pygame
 
 class VierOpEenRijGame():
-    
-    
+
+
     def __init__(self):
         pass
         #1
         pygame.init()
-        
+
         self.boardBoxH=7
         self.boardBoxW=7
-        
+
         self.boxD=100
         self.boxB=int(self.boxD/10)
 
-        width, height = self.boardBoxW*self.boxD - (self.boardBoxW-1)*self.boxB +self.boxB*4, self.boardBoxH*self.boxD-(self.boardBoxH-1)*self.boxB +self.boxB*4
+        width, height = self.boardBoxW*self.boxD - (self.boardBoxW-1)*self.boxB +self.boxB*4, self.boxD+self.boardBoxH*self.boxD-(self.boardBoxH-1)*self.boxB +self.boxB*4
         #2
         #initialize the screen
         self.screen = pygame.display.set_mode((width, height))
@@ -24,11 +24,19 @@ class VierOpEenRijGame():
         #initialize pygame clock
         self.clock=pygame.time.Clock()
         self.initGraphics()
-    
+
+        #defineer bord grote
+        self.board = [[0 for x in range(self.boardBoxW)] for y in range(self.boardBoxH)]
+
+        self.pijl=self.greenBox
+        self.pijlx=0
+        self.pijly=0
+
+
     def initGraphics(self):
         self.legeBox=pygame.transform.scale( pygame.image.load("legeBox.png"),(self.boxD,self.boxD))
         self.greenBox=pygame.transform.scale( pygame.image.load("greenBox.png"),(self.boxD,self.boxD))
-        self.orangeBox=pygame.transform.scale( pygame.image.load("orangeBox.png"),(self.boxD,self.boxD))
+        self.bleuBox=pygame.transform.scale( pygame.image.load("bleuBox.png"),(self.boxD,self.boxD))
 
     def update(self):
         #sleep to make the game 60 fps
@@ -43,20 +51,35 @@ class VierOpEenRijGame():
             if event.type == pygame.QUIT:
                 exit()
 
+            #pijl bewegen
+            if event.type == pygame.KEYDOWN :
+                if event.key==pygame.K_LEFT:
+                    if 0<self.pijlx:
+                        self.pijlx -= 1
+                if event.key==pygame.K_RIGHT:
+                    if self.pijlx<(self.boardBoxW-1):
+                        self.pijlx += 1
+
+        #plaats pijl
+        self.screen.blit(self.pijl,( (self.boxB*2)+((self.pijlx)*self.boxD)-self.boxB*self.pijlx, (self.boxB*2)+((self.pijly)*self.boxD)-self.boxB*self.pijly))
+
+
         #update the screen
         pygame.display.flip()
 
-        #defineer bord grote
-        
-        self.board = [[False for x in range(self.boardBoxW)] for y in range(self.boardBoxH)]
-    
+
+
     def drawBoard(self):
         for x in range(self.boardBoxW):
             for y in range(self.boardBoxH):
-                #if not self.board[y][x]:
-                self.screen.blit(self.legeBox, [(self.boxB*2)+((x)*self.boxD)-self.boxB*x, (self.boxB*2)+((y)*self.boxD)-self.boxB*y])
-        
-        
+                if self.board[y][x]==0:
+                    self.screen.blit(self.legeBox, [(self.boxB*2)+((x)*self.boxD)-self.boxB*x, self.boxD+(self.boxB*2)+((y)*self.boxD)-self.boxB*y])
+                if self.board[y][x]==1:
+                    self.screen.blit(self.greenBox, [(self.boxB*2)+((x)*self.boxD)-self.boxB*x, self.boxD+(self.boxB*2)+((y)*self.boxD)-self.boxB*y])
+                if self.board[y][x]==2:
+                    self.screen.blit(self.bleuBox, [(self.boxB*2)+((x)*self.boxD)-self.boxB*x, self.boxD+(self.boxB*2)+((y)*self.boxD)-self.boxB*y])
+
+
 bg=VierOpEenRijGame() #__init__ is called right here
 while 1:
     bg.update()
